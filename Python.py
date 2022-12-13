d3b65d5dd4c0c2902b59596a990bfc8fa49cce4b
Zein Tarhini <69736356+yungmaz13@users.noreply.github.com>
2022-12-13 12:23:20 +0000

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,39 +1,38 @@
-import turtle as g
+import turtle as t
 
 colours=["black","grey","white","red","yellow","pink","green","orange","violet","purple","blue","aqua","lime"]
 colour=colours[0]
 
-s=g.Screen()
-s.setup(1.0,1.0)
-s.title("Paint")
-s.bgcolor("white")
+Screen=t.Screen()
+Screen.setup(1.0,1.0)
+Screen.title("Paint")
+Screen.bgcolor("white")
 
-Map=[[str()for _ in range(s.window_width())]for _ in range(s.window_height())]
+Canvas=Screen.getcanvas()
 
-c=s.getcanvas()
-
-p=g.Turtle("circle")
-p.color(colour)
-p.speed(0)
-p.penup()
-
-corrector=p.clone()
-corrector.ht()
+Brush=t.Turtle("circle")
+Brush.color(colour)
+Brush.speed(0)
+Brush.penup()
 
 def addWidth():
-    p.shapesize(p.shapesize()[0]+1)
+    Brush.shapesize(Brush.shapesize()[0]+1)
 def addHeight():
-    p.shapesize(stretch_len=p.shapesize()[1]+1)
+    Brush.shapesize(stretch_len=Brush.shapesize()[1]+1)
 def addBoth():
-    p.shapesize(p.shapesize()[0]+1,p.shapesize()[1]+1)
+    Brush.shapesize(Brush.shapesize()[0]+1,Brush.shapesize()[1]+1)
 
 def minusWidth():
-    if p.shapesize()[0]>1:p.shapesize(p.shapesize()[0]-1)
+    if Brush.shapesize()[0]>1:
+        Brush.shapesize(Brush.shapesize()[0]-1)
 def minusHeight():
-    if p.shapesize()[1]>1:p.shapesize(stretch_len=p.shapesize()[1]-1)
+    if Brush.shapesize()[1]>1:
+        Brush.shapesize(stretch_len=Brush.shapesize()[1]-1)
 def minusBoth():
-    if p.shapesize()[0]>1:p.shapesize(p.shapesize()[0]-1)
-    if p.shapesize()[1]>1:p.shapesize(p.shapesize()[1]-1)
+    if Brush.shapesize()[0]>1:
+        Brush.shapesize(Brush.shapesize()[0]-1)
+    if Brush.shapesize()[1]>1:
+        Brush.shapesize(Brush.shapesize()[1]-1)
 
 draw=False
 def penup(x,y):
@@ -44,20 +43,21 @@ def pendown(x,y):
     draw=True
 
 def pixeltocartesian(x:float,y:float):
-    w,h=c.winfo_width(),c.winfo_height()
+    w,h=Canvas.winfo_width(),Canvas.winfo_height()
     xp,xc=list(range(w+1)),list(range(-w//2,w//2+1))
     yp,yc=list(range(h+1)),list(range(-h//2,h//2+1))
     return xc[xp.index(x)]-2,-yc[yp.index(y)]+20
 
 def blink(x,y):
-    if p.isvisible():p.ht()
-    else:p.st()
+    if Brush.isvisible():
+        Brush.ht()
+    else:
+        Brush.st()
 
 def dragging(x,y):
-    p.ondrag(None)
-    p.stamp()
-    Map[p.ycor()][p.xcor()]=p.fillcolor()
-    p.ondrag(dragging)
+    Brush.ondrag(None)
+    Brush.stamp()
+    Brush.ondrag(dragging)
 
 def switchcolour():
     global colour
@@ -65,17 +65,23 @@ def switchcolour():
         colour=colours[colours.index(colour)+1]
     except IndexError:
         colour=colours[0]
-    p.color(colour)
+    Brush.color(colour)
 
-s.onkeypress(switchcolour,"z")
-p.ondrag(dragging)
-s.onscreenclick(blink,2)
-for i,j in enumerate((minusWidth,addWidth,minusHeight,addHeight,minusBoth,addBoth),1):s.onkeypress(j,str(i))
-s.onkeypress(p.clear,"c")
-for i in "q","e":s.onkeypress(s.bye,i)
-s.listen()
+Screen.onkeypress(switchcolour,"z")
+Brush.ondrag(dragging)
+Screen.onscreenclick(blink,2)
+for i,j in enumerate((minusWidth,addWidth,minusHeight,addHeight,minusBoth,addBoth),1):
+    Screen.onkeypress(j,str(i))
+Screen.onkeypress(Brush.clear,"c")
+for i in "q","e":
+    Screen.onkeypress(Screen.bye,i)
+
+Screen.listen()
 while True:
     try:
-        p.goto(pixeltocartesian(*c.winfo_pointerxy()))
-        s.update()
-    except Exception as e:print(e)
+        Brush.goto(pixeltocartesian(*Canvas.winfo_pointerxy()))
+        Screen.update()
+    except Exception as e:
+        print(e)
+
+print()
